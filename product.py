3fdfcb813f5aa366748c21e0719e11f9c32ffeb9
Valentin Dan <160849517+VallyDvG@users.noreply.github.com>
2025-12-05 02:00:08 +0200

    Add Product and Inventory classes
    Introduces Product, Inventory, and Bank classes to model store products and inventory management. Includes methods for selling products, displaying product info, and managing inventory. Adds example usage for creating products and adding them to inventory.

---
+++ product.py
@@ -0,0 +1,69 @@
+class Product:
+    """Class that represents a product in a store""" 
+    def __init__(self, name: str, price: float, stock: int):# Initialize Product with name, price, and stock
+        self.name = name
+        self.price = price
+        self.stock = stock
+        self.money =0
+    
+    def is_in_stock(self)->bool:
+        if self.stock >0:
+            return True
+        else:
+            return False
+        
+    def sell(self,quantity:float):
+        if self.stock > quantity:
+            self.stock-=quantity
+            self.money=self.price * quantity
+        else:
+            print(f"You cannot buy {quantity} items, there are left {self.stock} items in stock")
+        return self.money
+
+    def product_info(self):
+        return f"There are left in stock {self.stock} items of {self.name}."
+
+
+class Inventory:
+    """Class that represents an inventory of products."""
+    def __init__(self):
+        self.products:dict[str,Product] = {}
+    
+    def add_product(self,product:Product):
+        self.products[product.name] = product
+
+    def show_inventory(self):
+        for product in self.products.values():
+            print(product.product_info())
+
+    def show_product(self,product_name:str):
+        pass
+
+    def list_products(self):
+        for product in self.products.values():
+            print(product.product_info())
+            
+            
+
+
+class Bank:
+    def __init__(self):
+        pass
+
+
+steak=Product("steak",75.3,15)
+milk=Product("milk",7.5,50)
+bread=Product("bread",3.5,30)
+
+inventory=Inventory()
+
+inventory.add_product(bread)
+inventory.add_product(steak)
+inventory.add_product(milk)
+
+# print(milk.product_info())
+inventory.list_products
+milk.sell(20)
+# print(milk.product_info())
+(inventory.list_products())
+

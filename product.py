f244ac8787fb66d144b106b8158e6b078ad19496
Valentin Dan <dvg_vally@yahoo.com>
2025-12-05 12:40:58 +0200

    Enhance product info and add Bank class implementation
    Updated Product methods to provide more detailed information. Implemented the Bank class with balance management and added example usage. Improved output formatting for product display.

--- product.py
+++ product.py
@@ -21,10 +21,10 @@ class Product:
         return self.money
 
     def product_info(self):
-        return f"There are left in stock {self.stock} items of {self.name}."
+        return f"There are left in stock {self.stock} items of {self.name} and it costs {self.price}."
 
     def __str__(self):
-        return f"Product: {self.name}, {self.stock}"
+        return f"Product: {self.name}, {self.stock} available"
 
 class Inventory():
     """Class that represents an inventory of products."""
@@ -43,35 +43,48 @@ class Inventory():
             print(self.products[product_name])   
         except KeyError as e:
             print(f"Product: {e} is not in stock! ")
-            #print(e)
-    def list_products(self):
-        for product in self.products.values():
-            print(product.product_info())
-            
+
             
 
 
-class Bank:
+class Bank():
     def __init__(self):
-        pass
+        self.balance=0
+    
+    def add_balance(self,money:float):
+        self.balance+=money
 
 
 steak=Product("steak",75.3,15)
 milk=Product("milk",7.5,50)
 bread=Product("bread",3.5,30)
 
-
+bank = Bank()
 inventory=Inventory()
+
 inventory.add_product(bread)
 inventory.add_product(steak)
 inventory.add_product(milk)
-
-inventory.show_product("steak")
-steak.sell(2)
+inventory.show_inventory()
 inventory.show_product("steak")
 
+steak.sell(14)
+
+inventory.show_product("steak")
+inventory.show_inventory()
+
+
+
+# print(bank.balance)
+# price=steak.sell(2)
+# bank.add_balance(price)
+# print(bank.balance)
+# bank.add_balance(price)
+# print(bank.balance)
+#inventory.show_product("steak")
+# print(bank)
+# inventory.show_product("steak")
 # inventory.show_product("milk")
-
 # print(milk.product_info())
 # inventory.list_products()
 # milk.sell(20)
